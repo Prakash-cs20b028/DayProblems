ad68bb0e51e61cb1b6de40b1b01cb936b8609270
Prakash-cs20b028 <Programmer52525@gmail.com>
2024-02-19 23:04:16 +0530

    d19

---
+++ feb_d19.cpp
@@ -0,0 +1,14 @@
+bool isPowerOfTwo(int n) {
+        
+        int count=0;
+        if(n<=0){return false;}
+        while(n>0)
+        {
+            if(n&1 == 1)
+            {
+                count++;
+            }
+            n = n>>1;
+        }
+        return (count>1)?false:true;
+    } 
