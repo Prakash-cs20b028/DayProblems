d1cf6b03429f734c0879e62ce5de91ab6f6f41dd
Prakash-cs20b028 <Programmer52525@gmail.com>
2024-02-26 16:39:38 +0530

    d26

---
+++ feb_d26.cpp
@@ -0,0 +1,65 @@
+ 
+vector<string> ans;
+        void fun(string str,int n)
+        {
+            int high = 1<<n;
+            for(int i=0;i<high;i++)
+            {
+                string temp="";
+                
+                for(int j=0;j<n;j++)
+                {
+                    
+                    if((i&(1<<j)) != 0)
+                    {
+                        temp += str[j];
+                       
+                    }
+                    
+                }
+                ans.push_back(temp);
+               
+            }
+        }
+        vector<string> AllPossibleStrings(string s){
+            
+            
+            fun(s,s.length());
+            sort(ans.begin(),ans.end());
+            ans.erase(ans.begin()+0);
+            return ans;
+        }
+
+ 
+
+Recursion version
+
+ 
+
+vector<string> ans;
+        void fun(string str,string output)
+        {
+            if(str.length() == 0)
+            {
+                ans.push_back(output);
+                return;
+            }
+            else
+            {
+                string str1=output;  //str1 left side
+                string str2=output;  //str2 right side
+                
+                str2.push_back(str[0]);
+                str.erase(str.begin()+0);
+                fun(str,str1);
+                fun(str,str2);
+            }
+        }
+        vector<string> AllPossibleStrings(string s){
+            
+            string out = "";
+            fun(s,out);
+            sort(ans.begin(),ans.end());
+            ans.erase(ans.begin()+0);
+            return ans;
+        }
