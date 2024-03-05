9f2424faea1f9e712463010b9582d7ce8e3e2c5d
Prakash-cs20b028 <Programmer52525@gmail.com>
2024-03-05 18:22:26 +0530

    d5

--- mar_d5.cpp
+++ mar_d5.cpp
@@ -1,9 +1,33 @@
  
-void swapElements(int arr[], int n){
-        
-        for(int i=0;i<n-2;i++)
+int maxIndexDiff(int arr[], int n) 
+    { 
+        int ans=0;
+        vector<int>vec1(n),vec2(n);
+        int maxi = 0;
+        for(int i=n-1;i>=0;i--)
         {
-            swap(arr[i],arr[i+2]);
+            maxi = max(maxi,arr[i]);
+            vec1[i] = maxi;
         }
-        
+        int mini = arr[0];
+        for(int i=0;i<n;i++)
+        {
+            mini = min(mini,arr[i]);
+            vec2[i] = mini;
+        }
+        // Main implementation. Assume j for vec1 and i for vec2;
+        int i=0,j=0;
+        while(i<n and j<n)
+        {
+            if(vec2[i]<=vec1[j])
+            {
+                ans = max(ans,j-i);
+                j++;
+            }
+            else
+            {
+                i++;
+            }
+        }
+        return ans;
     }
