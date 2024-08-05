7bcb7cde66a65d459cb611c469faeb60204be51f
Prakash-cs20b028 <Programmer52525@gmail.com>
2024-08-06 00:19:25 +0530

    Code added

---
+++ aug_d6.cpp
@@ -0,0 +1,34 @@
+vector <int> bottomView(Node *root) 
+    {
+        
+        vector <int> ans;
+        if(root==NULL)
+            return ans;
+        map<int,int>mp;
+        
+        queue<pair<Node*,int>>q;
+        q.push({root,0});
+        
+        while(!q.empty())
+        {
+            Node* node = q.front().first;
+            int line=q.front().second;
+            q.pop();
+            
+            mp[line]=node->data;
+
+            if(node->left!=NULL)
+            {
+                q.push({node->left,line-1});
+            }
+            if(node->right!=NULL)
+            {
+                q.push({node->right,line+1});
+            }
+        }
+        for(auto it:mp)
+        {
+            ans.push_back(it.second);
+        }
+        return ans;
+    }
